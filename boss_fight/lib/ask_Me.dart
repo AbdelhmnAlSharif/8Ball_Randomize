425329466c786f2b48bb39ae39b965d5d2f9594d
AbdelhmnAlSharif <Abedelrahmanalsharif31@gmail.com>
2024-02-19 18:27:30 +0200

    A game for rest of time

---
+++ boss_fight/lib/ask_Me.dart
@@ -0,0 +1,40 @@
+import "package:flutter/material.dart";
+import 'dart:math';
+
+class AskMe extends StatelessWidget {
+  const AskMe({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: const Text("Ask Me Anything"),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: (){
+          setState(() {
+  ballNumber = Random().nextInt(5) + 1;
+    });
+        },
+      child: Image.asset('../images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
